afa9f5a530e0e1bb0a8239f474ce27168e3178a6
Andreas Plado <andreasplado@gmail.com>
2021-09-26 22:44:40 +0300

    Settings

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,35 +1,98 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
-class Contact extends StatelessWidget {
+// Define a custom Form widget.
+class Contact extends StatefulWidget {
   const Contact({Key? key}) : super(key: key);
 
   @override
+  ContactState createState() {
+    return ContactState();
+  }
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+class ContactState extends State<Contact> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
+
+  @override
   Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Text('Kontakt'),
-          ],
-        ),
-
-
-        backgroundColor: Colors.blue[400],
-        actions: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0.0, 0.0, 16.0, 0.0),
-            child: IconButton(
-              icon: Icon(
-                Icons.call,
-                size: 32.0,
+        body: Padding(padding: EdgeInsets.fromLTRB(20, 0, 20, 0),child: Column(
+      children: <Widget>[
+        FormBuilder(
+          key: _fbKey,
+          child: Column(
+            children: <Widget>[
+              FormBuilderDateTimePicker(
+                inputType: InputType.date,
+                format: DateFormat("yyyy-MM-dd"),
+                decoration: InputDecoration(labelText: "Appointment Time"),
+                name: 'k',
               ),
-              onPressed: () {
-              },
-            ),
+              FormBuilderSlider(
+                min: 0.0,
+                max: 10.0,
+                initialValue: 1.0,
+                divisions: 20,
+                decoration: InputDecoration(labelText: "Number of somethings"),
+                name: 'h',
+              ),
+              FormBuilderCheckbox(
+                initialValue: false,
+                name: 'kjkk',
+                title: Text("rf"),
+              ),
+              FormBuilderDropdown(
+                decoration: InputDecoration(labelText: "Gender"),
+                // initialValue: 'Male',
+                hint: Text('Select Gender'),
+                items: ['Male', 'Female', 'Other']
+                    .map((gender) =>
+                        DropdownMenuItem(value: gender, child: Text("$gender")))
+                    .toList(),
+                name: 'cddvvd',
+              ),
+              FormBuilderTextField(
+                decoration: InputDecoration(labelText: "Age"),
+                name: 'dddd',
+              ),
+              FormBuilderSegmentedControl(
+                decoration: InputDecoration(labelText: "Movie Rating (Archer)"),
+                options: List.generate(5, (i) => i + 1)
+                    .map((number) => FormBuilderFieldOption(value: number))
+                    .toList(),
+                name: 'jhju',
+              ),
+              FormBuilderSwitch(
+                initialValue: true,
+                name: 'hjhjhhj',
+                title: Text("fff"),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ),
+        Row(
+          children: <Widget>[
+            MaterialButton(
+              onPressed: () {},
+              child: Text("Submit"),
+            ),
+            MaterialButton(
+              child: Text("Reset"),
+              onPressed: () {},
+            ),
+          ],
+        )
+      ],
+    )));
   }
 }
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class Settings extends StatelessWidget {
   const Settings({Key? key}) : super(key: key);
@@ -6,11 +7,12 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         title: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: const [
-            Text('Kontakt'),
+            Text('Seaded'),
           ],
         ),
 
@@ -31,7 +33,60 @@ class Settings extends StatelessWidget {
 
 
         ],
+
+
+      ),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            titlePadding: EdgeInsets.all(20),
+            title: 'Põhiseaded',
+            tiles: [
+              SettingsTile(
+                title: 'Keel',
+                subtitle: 'Inglise',
+                leading: Icon(Icons.language),
+                onPressed: (BuildContext context) {},
+              ),
+              SettingsTile.switchTile(
+                title: 'Kasuta tumedat teemat',
+                leading: Icon(Icons.phone_android),
+                onToggle: (value) {
+                  setState(() {
+                  });
+                }, switchValue: true,
+              ),
+            ],
+          ),
+          SettingsSection(
+            titlePadding: EdgeInsets.all(20),
+            title: 'Turvaseaded',
+            tiles: [
+              SettingsTile(
+                title: 'Muuda parooli',
+                subtitle: 'Sea uus parool',
+                leading: Icon(Icons.password),
+                onPressed: (BuildContext context) {},
+              ),
+              SettingsTile.switchTile(
+                title: 'Kasuta biomeetrilist autentimist',
+                leading: Icon(Icons.fingerprint),
+                switchValue: true,
+                onToggle: (value) {},
+              ),
+              SettingsTile(
+                title: 'Kasutaja kustutamine',
+                subtitle: 'Kustuta kasutaja',
+                leading: Icon(Icons.delete_forever),
+                onPressed: (BuildContext context) {},
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
+
+  void setState(Null Function() param0
+      ) {}
 }
